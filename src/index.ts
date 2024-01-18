18f362bc1f727feca24b043f639c596f85b92888
Nana Awuku <apatrickadjei@gmail.com>
2024-01-18 11:01:13 +0000

    routers commit

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,25 @@
-import express from "express";
-import mongoose from "mongoose";
+// server.ts
+
+import express from 'express';
 import imageRoutes from './routes/imageRoutes';
-import dotenv from "dotenv";
-import { connectToDatabase } from "./database/connection";
+import dotenv from 'dotenv';
+import { connectToDatabase } from './database/connection';
+import { errorHandler } from './middleware/errorMiddleware';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.SERVER_PORT || 6000;
 
-
-
-
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-// Define your routes here
 app.use('/api', imageRoutes);
 
-// Error handling middleware
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).json({ error: 'Internal server error' });
-// });
+// Error Handling Middleware
+app.use(errorHandler);
 
 // Start server after MongoDB connection
 const startServer = async () => {
---
+++ src/middleware/errorMiddleware.ts
@@ -0,0 +1,18 @@
+// errorMiddleware.ts
+
+import { Request, Response, NextFunction } from 'express';
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.error(err.stack);
+
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  res.status(500).json({ error: 'Internal server error' });
+};
